418793095f7cd5dcaccb144b27264ce3e6cad704
minhnguyenbinh <minhnguyenbinh2703@gmail.com>
2017-12-11 23:08:30 +0700

    sab2

--- app/footer.php
+++ app/footer.php
@@ -3,21 +3,21 @@
   	<div class="row">
   		<div class="col-md-5 ft">
   			<h5> otobinhminh.com</h5>
-  			<p>-20 Ba Trieu, Thua Thien Hue</p>
-  			<p>-SDT: 01684162805</p>
-  			<p>-Email: minhnguyebinh2703@gmail.com</p>
+  			<p>- 20 Bà Triệu, Tp. Huế, Thừa Thiên Huế</p>
+  			<p>- SDT: 01684162805</p>
+  			<p>- Email: minhnguyebinh2703@gmail.com</p>
   		</div>
   	    <div class="col-md-4 ft">
   	        <h5> ĐỊA CHỈ LIÊN HỆ</h5>
-  	        <p>-20 Ba Trieu, Thua Thien Hue</p>
-  			<p>-SDT: 01684162805</p>
-  			<p>-Email: minhnguyebinh2703@gmail.com</p>
+  	        <p>- 20 Bà Triệu, Tp. Huế, Thừa Thiên Huế</p>
+  			<p>- SDT: 01684162805</p>
+  			<p>- Email: minhnguyebinh2703@gmail.com</p>
   		</div>
   	    <div class="col-md-3 ft">
   	        <h5>THÔNG TIN</h5>
-  	        <p>-20 Ba Trieu, Thua Thien Hue</p>
-  			<p>-SDT: 01684162805</p>
-  			<p>-Email: minhnguyebinh2703@gmail.com</p>
+  	        <p>- 20 Bà Triệu, Tp. Huế, Thừa Thiên Huế</p>
+  			<p>- SDT: 01684162805</p>
+  			<p>- Email: minhnguyebinh2703@gmail.com</p>
   	    </div>
     </div>
   </div>
