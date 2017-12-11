3fc1765348d7d05b28b5a5787f4a72cc824a2e51
minhnguyenbinh <minhnguyenbinh2703@gmail.com>
2017-12-11 23:03:10 +0700

    sad

--- app/about.php
+++ app/about.php
@@ -8,7 +8,7 @@
   <?php include "menu.php" ?>
   <div class="container">
     <h3 class="h3-gt">GIỚI THIỆU</h3><br><br>
-    <p class="p-gt">gia re nhat hue, chat luong tot nhat hue</p>
+    <p class="p-gt">Ngày nay, việc sở hữu một chiếc ô tô không còn là điều quá xa vời với người Việt khi trên thị trường xuất hiện ngày càng nhiều dòng ô tô giá rẻ. Những chiếc xe giá rẻ này đa phần đều có thiết kế khá bắt mắt, đơn giản hiện đại nhưng không kém phần tiện dụng. Bên cạnh đó, mức giá hợp túi tiền với số đông người Việt cùng việc sửa chữa và bảo dưỡng dễ dàng càng ghi điểm hơn trong mắt khách hàng.</p>
   </div>
 
 <?php include "footer.php" ?>
